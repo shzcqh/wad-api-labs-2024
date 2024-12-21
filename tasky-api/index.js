92122a191d442e407dd7e2b6e17245eb36c7ec54
unknown <2867459421@qq.com>
2024-12-21 22:53:43 +0800

    Mongo lab; Included Mongoose

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,17 +1,42 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/tasks';
+import Task from './taskModel';
 
-dotenv.config();
+const router = express.Router(); // eslint-disable-line
 
-const app = express();
-
-const port = process.env.PORT;
-
-app.use(express.json());
-
-app.use('/api/tasks', tasksRouter);
-
-app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+// Get all tasks
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
 });
+
+// create a task
+router.post('/', async (req, res) => {
+    const task = await Task(req.body).save();
+    res.status(201).json(task);
+});
+// Update Task
+router.put('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Task.updateOne({
+        _id: req.params.id,
+    }, req.body);
+    if (result.matchedCount) {
+        res.status(200).json({ code:200, msg: 'Task Updated Successfully' });
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
+    }
+});
+
+// delete Task
+router.delete('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Task.deleteOne({
+        _id: req.params.id,
+    });
+    if (result.deletedCount) {
+        res.status(204).json();
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
+    }
+});
+export default router;
